115a7e4884567c78c1a39851f87c710a88bbcd77
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-02-07 11:02:08 +0200

    use buffer instead

--- app/main.js
+++ app/main.js
@@ -6,25 +6,25 @@ const command = process.argv[3];
 if (command === ".dbinfo") {
   const databaseFileHandler = await open(databaseFilePath, "r");
 
-  const { DBHeader } = await databaseFileHandler.read({
-    length: 100,
+  const { buffer } = await databaseFileHandler.read({
+    length: 112,
     position: 0,
-    buffer: Buffer.alloc(100),
+    buffer: Buffer.alloc(112),
   });
-  const { pageHeader } = await databaseFileHandler.read({
-    length: 12,
-    position: 100,
-    buffer: Buffer.alloc(12),
-  });
+  // const { pageHeader } = await databaseFileHandler.read({
+  //   length: 12,
+  //   position: 100,
+  //   buffer: Buffer.alloc(12),
+  // });
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   console.log("Logs from your program will appear here!");
 
   // Uncomment this to pass the first stage
-  const pageSize = DBHeader.readUInt16BE(16); // page size is 2 bytes starting at offset 16
+  const pageSize = buffer.readUInt16BE(16); // page size is 2 bytes starting at offset 16
   console.log(`database page size: ${pageSize}`);
-  const numOfTables = pageHeader.readUInt16BE(3);
+  const numOfTables = buffer.readUInt16BE(103);
   console.log(`number of tables: ${numOfTables}`);
-  console.log(pageHeader.readUInt16BE(0));
+  console.log(buffer.readUInt16BE(100));
 } else {
   throw `Unknown command ${command}`;
 }
